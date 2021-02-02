f060093357864b7c8f3cd365c171046b44aa9c63
frozentiger279 <55792108+frozentiger279@users.noreply.github.com>
2021-02-02 09:45:48 -0400

    Intitial Commit

---
+++ EnemyAI/Assets/Scripts/EnemyBasic.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBasic : MonoBehaviour
+{
+    public int moveSpeed = 3;
+    int direction = 1;
+    Vector3 position;
+
+    // Update is called once per frame
+    void Update()
+    {
+        position.x = direction * moveSpeed * Time.deltaTime;
+        transform.Translate(position);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        direction = direction * -1;
+    }
+}
